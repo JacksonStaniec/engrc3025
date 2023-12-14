ac6a77ae3e4daa72fb1ad4deb8c2e6dc3ba5dec7
jks273 <jks273@cornell.edu>
2023-12-14 14:26:41 -0700

    Remove arrow

--- components/GanttChart/GanttChart.tsx
+++ components/GanttChart/GanttChart.tsx
@@ -1,5 +1,4 @@
-import { IconArrowDown, IconArrowRight } from '@tabler/icons-react';
-import { Center, Container, Grid, Stack, Text } from '@mantine/core';
+import { Container, Grid, Stack, Text } from '@mantine/core';
 import { Timeline } from '../Timeline/Timeline';
 import classes from './GanttChart.module.css';
 import { TimeAxis } from './TimeAxis';
@@ -24,16 +23,8 @@ export const GanttChart = ({
       <Stack h="100%" justify="space-between">
         {chartElements.map(({ title, range }, i) => (
           <Grid key={i} className={classes['column-arrangement']}>
-            <Grid.Col span={{ base: 12, md: 1 }}>
-              <Center>
-                <div hidden={i !== selectedIndex}>
-                  <IconArrowDown color={accentColor} className={classes['icon-mobile']} />
-                  <IconArrowRight color={accentColor} className={classes['icon-desktop']} />
-                </div>
-              </Center>
-            </Grid.Col>
-            <Grid.Col span={{ base: 1, md: 0 }} />
-            <Grid.Col span={{ base: 7, md: 7 }}>
+            <Grid.Col span={{ base: 1 }} />
+            <Grid.Col span={{ base: 7 }}>
               <Timeline
                 accentColor={accentColor}
                 key={i}
@@ -43,18 +34,17 @@ export const GanttChart = ({
                 clickCallback={selectCallback}
               />
             </Grid.Col>
-            <Grid.Col span={{ base: 4, md: 3 }}>
+            <Grid.Col span={{ base: 4 }}>
               <Text>{title}</Text>
             </Grid.Col>
           </Grid>
         ))}
         <Grid pb="lg">
-          <Grid.Col span={{ base: 12, md: 1 }} />
-          <Grid.Col span={{ base: 1, md: 0 }} />
-          <Grid.Col pb="lg" span={{ base: 7, md: 7 }}>
+          <Grid.Col span={{ base: 1 }} />
+          <Grid.Col pb="lg" span={{ base: 7 }}>
             <TimeAxis />
           </Grid.Col>
-          <Grid.Col span={{ base: 2, md: 3 }} />
+          <Grid.Col span={{ base: 4 }} />
         </Grid>
       </Stack>
     </Container>

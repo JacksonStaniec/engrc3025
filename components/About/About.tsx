93b69dec87798de63eb982c6d25ea692e151a12c
jks273 <jks273@cornell.edu>
2023-12-14 18:33:51 -0700

    Shorten about me and highlight key words

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,23 +2,25 @@ import { Container, Stack, Text } from '@mantine/core';
 import { Socials } from '../Socials/Socials';
 import classnames from './About.module.css';
 
-const aboutText = `
-Hi! I'm currently a senior at Cornell University in the College of Engineering, 
-and my goal is to find a specialization in the broad field of computer science 
-once I graduate. 
-
-I'm always looking for ways to improve my skills, and I think mentorship from more 
-experienced engineers is one of the best opportunities for that. 
-I like to get involved in the non-engineering aspects of projects as well, so I'd 
-be interested in participating in discussions about end-user experience or the 
-business needs that the project addresses.
-`;
-
 export const About = () => (
   <Container m="md" px="xl" maw="100%">
     <Stack>
       <Socials />
-      <Text className={classnames.about}>{aboutText}</Text>
+      <div className={classnames.about}>
+        <Text span>
+          Hi! I&apos;m currently a senior at <b>Cornell University</b> in the College of
+          Engineering.{' '}
+        </Text>
+        <Text span>
+          I&apos;m always looking for ways to improve my skills, and I think <b>mentorship</b> from
+          more experienced engineers is one of the best opportunities for that.{' '}
+        </Text>
+        <Text span>
+          I like to get involved in the <b>non-engineering</b> aspects of projects as well, so
+          I&apos;d be interested in participating in discussions about end-user experience or the
+          business needs that the project addresses.
+        </Text>
+      </div>
     </Stack>
   </Container>
 );
